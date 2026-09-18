1eff25e49d5f1c1d6c6bc750713f2ab8d6f0d636
NikaDogonadze333 <nika.dogo123@gmail.com>
2026-09-18 20:31:33 +0400

    Lecture 2 homework

---
+++ lecture2/variables_and_types.py
@@ -0,0 +1,9 @@
+user_name = "Alex"
+user_age = 20
+user_height = 1.75
+is_student = True
+
+print(user_name, type(user_name))
+print(user_age, type(user_age))
+print(user_height, type(user_height))
+print(is_student, type(is_student))
