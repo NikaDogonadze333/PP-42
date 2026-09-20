057198e94460b119b5c36f000e3ea60baed4610a
NikaDogonadze333 <nika.dogo123@gmail.com>
2026-09-20 16:57:02 +0400

    Lecture 3 homework

---
+++ lecture3/text_work.py
@@ -0,0 +1,10 @@
+text = "My favorite thing is Python"
+
+new_text = text.replace("thing", "language")
+print(new_text)
+
+index = text.find("Python")
+print(index)
+
+sliced_text = text[12:]
+print(sliced_text)
---
+++ lecture3/workspace.py
@@ -0,0 +1,5 @@
+name = input("Enter name: ")
+company = input("Enter company: ")
+
+message = f"Hello {name}, your workspace is {company}."
+print(message)
